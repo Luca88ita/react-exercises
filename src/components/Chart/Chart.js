e6df265ea261bb4690979a5a0b4aa998277b81ac
Luca88ita <luca198871@gmail.com>
2023-03-11 18:41:45 +0100

    section 5 completed

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import "./Chart.css";
+import ChartBar from "./ChartBar";
+
+const Chart = (props) => {
+  const dataPointValues = props.dataPoints.map((dataPoint) => dataPoint.value);
+  //const totalMaximum = Math.max(...dataPointValues);
+  const totalMaximum = dataPointValues.reduce((a, b) => a + b, 0);
+
+  return (
+    <div className="chart">
+      {props.dataPoints.map((dataPoint) => (
+        <ChartBar
+          key={dataPoint.label}
+          value={dataPoint.value}
+          maxValue={totalMaximum}
+          label={dataPoint.label}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Chart;
--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -40,7 +40,7 @@ const ExpenseForm = (props) => {
 
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     };
 
@@ -83,6 +83,9 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className="new-expense__actions">
+        <button type="reset" onClick={props.onCancel}>
+          Cancel
+        </button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
