e9616585fc50b84e0a6daf91c1cafc985fcc75c0
Luca88ita <luca198871@gmail.com>
2023-03-11 13:55:45 +0100

    update with conditional rendering on Expenses.js

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,47 +1,31 @@
 import React, { useState } from "react";
 import "./Expenses.css";
 
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2023");
 
-  /*let expArray = props.expenses;
-  const toReturn = [];
-
-  expArray.forEach((element) => {
-    toReturn.push(
-      <ExpenseItem
-        title={element.title}
-        amount={element.amount}
-        date={element.date}
-        key={element.id}
-      />
-    );
-  });*/
+  const filteredExpenses = props.expenses.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
 
   const saveYearHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
 
   return (
-    <Card className="expenses">
-      <ExpensesFilter
-        selectedYear={filteredYear}
-        onYearChange={saveYearHandler}
-      />
-      {props.expenses.map((expense) => (
-        <ExpenseItem
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-          key={expense.id}
+    <li>
+      <Card className="expenses">
+        <ExpensesFilter
+          selectedYear={filteredYear}
+          onYearChange={saveYearHandler}
         />
-      ))}
-      {/*toReturn*/}
-    </Card>
+        <ExpensesList expenses={filteredExpenses} />
+      </Card>
+    </li>
   );
 };
 
